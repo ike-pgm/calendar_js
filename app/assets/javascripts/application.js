fee61a6f075fd23e5ce57881ea7ff84cd917e431
github ike-pgm <ike.cut.pgm@gmail.com>
2018-11-23 18:38:53 +0900

    calendar-js

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,13 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery
+//= require moment
+//= require fullcalendar
+//= require fullcalendar/lang/ja
+//= require_tree .
+
+console.log('hoge');
+$(document).ready(function() {
+ $('#calendar').fullCalendar({});
+});
---
+++ app/assets/javascripts/calendars.coffee
@@ -0,0 +1,3 @@
+# $(document).on 'turbolinks:load', ->
+#   $('#calendar').fullCalendar {}
+#   return
